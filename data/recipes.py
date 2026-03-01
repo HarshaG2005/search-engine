c82ab1c24505a27b7be2a4e55b22c28f021740b4
Harsha Gayantha <harshaa7654@gmail.com>
2026-03-01 10:32:59 +0530

    Created recipe.json

--- data/recipes.py
+++
@@ -1,7 +0,0 @@
-recipes = [
-    {"id": 1, "title": "Coconut Curry", "text": "coconut milk curry powder"},
-    {"id": 2, "title": "Prawn Curry", "text": "prawn in coconut curry isso"},
-    {"id": 3, "title": "Dhal Curry", "text": "a lentil curry coconut"},
-    {"id": 4, "title": "Isso Curry", "text": "isso prawn spicy curry"},
-    {"id": 5, "title": "Fish Ambul Thiyal", "text": "fish sour spicy in goraka"},
-]
