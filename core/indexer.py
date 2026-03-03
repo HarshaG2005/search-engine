8de139ae9b777033a4260d2285a69f4f1e7651e3
Harsha Gayantha <harshaa7654@gmail.com>
2026-03-03 09:35:23 +0530

    Refactor indexer to use collections.defaultdict for improved indexing and clean up unused code; add recipe.json for data source

--- core/indexer.py
+++ core/indexer.py
@@ -1,50 +1,24 @@
 import os
 import sys
 import json
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import collections
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import math
 from core.preprocesser import preprocess
+
 with open(os.path.join(os.path.dirname(__file__), "../data/recipe.json")) as f:
     recipes = json.load(f)
 
-
-
 def makeIndex(recipes):
-    index = {}
-    for i,recipe in enumerate(recipes.values()):
-        content = (
-            recipe["title"] + " " + " ".join(recipe["ingredients"])
-        )  # to include the title and ingredients in the search
+    index = collections.defaultdict(set)
+    for recipe in recipes.values():
+        content = recipe["title"] + " " + " ".join(recipe["ingredients"])
         words = preprocess(content)
         for word in words:
-            if word not in index:
-                index[word] = []
-            if recipe["id"] not in index[word]:
-                index[word].append(recipe["id"])
+           index[word].add(recipe["id"])
     return index
 
-
 indexes = makeIndex(recipes)
-
-
-# def search_2_words(word1, word2, indexes):
-#     word1 = word1.lower()  # to make the search case insensitive
-#     word2 = word2.lower()  # to make the search case insensitive
-#     word1 = ps.stem(word1)
-#     word2 = ps.stem(word2)
-#     ids1 = set(
-#         indexes.get(word1, [])
-#     )  # set(None) doesn't give an empty set — it crashes with a TypeError. set([]) gives an empty set. Small but important distinction to have right in your mental model.
-#     ids2 = set(indexes.get(word2, []))
-#     if ids1 and ids2:
-#         return list(ids1 & ids2)  # lists dont have intersection but sets do
-#     elif ids1:
-#         return list(ids1)
-#     elif ids2:
-#         return list(ids2)
-#     else:
-#         return None
-
 
 if __name__ == "__main__":
     print(makeIndex(recipes))
--- data/d.py
+++ data/d.py
@@ -1,7 +1,15 @@
-import json
-import os
+# import json
+# import os
 
-with open(os.path.join(os.path.dirname(__file__), "recipe.json")) as f:
-    recipes = json.load(f)
+# with open(os.path.join(os.path.dirname(__file__), "dt.json")) as f:
+#     recipes = json.load(f)
 
-print(recipes.values())
+# # recipe = recipes["12"]
+# # print(recipe["title"] + " " + " ".join(recipe["ingredients"]))
+# print(list(recipes.values())[0]["title"] + " " + " ".join(list(recipes.values())[0]["ingredients"]))
+
+# from collections import defaultdict
+# index=defaultdict(set)
+# index["coconut"]=[1,2]
+# index["coconu"].add(3)
+# print(index)
