55f1b78da744c404de7f62cd374b77705f0fe482
Harsha Gayantha <harshaa7654@gmail.com>
2026-03-08 10:50:20 +0530

    Implement indexing functionality and create JSON output for index, document lengths, and average document length; remove outdated run_index script

--- core/run_indexing.py
+++ core/run_indexing.py
@@ -5,10 +5,18 @@ import collections
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from core.indexer import build_index
 from core.preprocesser import preprocess
+from storage import save
 with open(os.path.join(os.path.dirname(__file__), "../data/recipe.json")) as f:
     recipes = json.load(f)
 
 
 
-def run_index():
-
+def run_indexing():
+    indexes,doc_len = build_index(recipes)
+    avg_doc_len = sum(doc_len.values()) / len(doc_len) if doc_len else 0
+    save(indexes, doc_len, avg_doc_len)
+    return True
+    
+if __name__ == "__main__":
+    run_indexing()
+    print("Indexing completed successfully.")
