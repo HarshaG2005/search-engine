2630d4e158cfbcef8d4f8b150f3e55aaaf3c12fe
Harsha Gayantha <harshaa7654@gmail.com>
2026-03-31 17:45:19 +0530

    feat(preprocess):Add common words i identified  ,to the stopword collection

--- core/preprocessor.py
+++ core/preprocessor.py
@@ -5,6 +5,7 @@ nltk.download("stopwords", quiet=True)
 from nltk.corpus import stopwords
 
 stopwords = set(stopwords.words("english"))
+sl_stopwords={"sri","lankan","srilankan","srilanka","lanka"}
 
 ps = PorterStemmer()
 
@@ -15,4 +16,5 @@ def preprocess(text):
     text = text.translate(str.maketrans("", "", string.punctuation))
     words = text.split()
     stemmed_words = [ps.stem(word) for word in words if word not in stopwords]
-    return stemmed_words
+    final_words=[word for word in stemmed_words if word not in sl_stopwords]
+    return final_words
