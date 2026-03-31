5b22372e5349d5e4aa17b2a821b6c341d4918dae
Harsha Gayantha <harshaa7654@gmail.com>
2026-03-31 12:22:32 +0530

    Update(run_indexing):update run_indexing func to use newly stored field informations about recipe titles and ingridients

--- core/run_indexing.py
+++ core/run_indexing.py
@@ -4,9 +4,10 @@ from core.storage import load_recipe, save
 
 def run_indexing():
     recipes = load_recipe()
-    indexes, doc_len, recipe_map, bigram_index = build_index(recipes)
+    indexes, doc_len, recipe_map, bigram_index,doc_field_len,avg_field_len = build_index(recipes)
     avg_doc_len = sum(doc_len.values()) / len(doc_len) if doc_len else 0
-    save(indexes, doc_len, avg_doc_len, recipe_map, bigram_index)
+    # dfs = {term: len(i[term].keys) for term, i in indexes.items()}
+    save(indexes, doc_len, avg_doc_len, recipe_map, bigram_index,doc_field_len,avg_field_len)
     return True
 
 
